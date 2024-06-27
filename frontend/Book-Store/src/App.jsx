96dc5100eb4b510b4d2046218334d4bc010f3b8c
ishaaaa8 <itsisha4884@gmail.com>
2024-06-27 18:31:43 +0530

    client side auth

--- frontend/Book-Store/src/App.jsx
+++ frontend/Book-Store/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Home from './home/Home'
 import Courses from './Courses/Courses'
 import {Route , Routes} from "react-router-dom"
+import Signup from './components/Signup'
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
           <Route
             path="/course"
             element={ <Courses /> } />
+
+          <Route path="/signup" element={<Signup/>} />
           
         </Routes>
         {/* <Toaster /> */}
